6029262f2a0e66f9c7ada1200fe2c53895a42e4a
Banyc <36535895+Banyc@users.noreply.github.com>
2020-04-24 01:57:14 +0800

    namespace conflict

--- src/MIPS.Organizer/Program.cs
+++ src/MIPS.Organizer/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using MIPS.Simulator;
+using MIPS.Interpreter;
+using MIPS.Shared;
 
-namespace MIPS.Console
+namespace MIPS.Organizer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
